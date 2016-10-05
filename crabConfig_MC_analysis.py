ffef0eb83f800ea387dcee0fdb50792a2f26ea8c
Matthew Walker <matthew.hh.walker@gmail.com>
2016-10-05 12:46:00 -0400

    added background dataset names

--- crabConfig_MC_analysis.py
+++ crabConfig_MC_analysis.py
@@ -8,8 +8,25 @@ number = 0
 
 # List of datasets
 datasetnames = [
-"/SingleElectron/Run2016G-PromptReco-v1/AOD", #0
-"/SingleMuon/Run2016G-PromptReco-v1/AOD",#1
+"/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM",#0
+"/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM",#1
+"/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v3/AODSIM",#2
+"/ST_s-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM",#3
+"/ST_t-channel_antitop_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM",#4
+"/ST_t-channel_top_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v3/AODSIM",#5
+"/ST_tW_antitop_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v2/AODSIM",#6
+"/ST_tW_top_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM",#7
+"/WH_HToBB_WToLNu_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring16reHLT80-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/AODSIM",#8
+"/DYJetsToLL_M-5to50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM",#9
+"/DYJetsToLL_BGenFilter_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM",#10
+"/WWTo2L2Nu_13TeV-powheg/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM",#11
+"/WZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM",#12
+"/WZTo3LNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM",#13
+"/WZTo1L1Nu2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM",#14
+"/ZZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM",#15
+"/ZZTo4L_13TeV-amcatnloFXFX-pythia8/RunIISpring16reHLT80-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/AODSIM",#16
+"/ZH_HToBB_ZToLL_M125_13TeV_powheg_pythia8/RunIISpring16reHLT80-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/AODSIM",#17
+"/ZHToTauTau_M125_13TeV_powheg_pythia8/RunIISpring16reHLT80-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/AODSIM",#18
 ]
 # Storage path for output files
 #storagepath = '/store/user/'+getUsernameFromSiteDB()+'/mwalker/NTUPLES/2016/Data'
@@ -50,7 +67,7 @@ config.JobType.pyCfgParams = ['outputFile='+OutputFilename]
 
 config.Data.inputDataset = datasetnames[number]
 config.Data.inputDBS = 'global'
-config.Data.splitting = 'LumiBased'
+config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 2
 config.Data.ignoreLocality = True
 config.Data.outLFNDirBase = storagepath
